c8bb52f5caa49e64a9ea1cdbc638a8eb19e160e0
Jacob Vincent <jacob.vincent@optum.com>
2019-08-20 14:27:02 -0500

    first run of train.py with DVC

--- train.py
+++ train.py
@@ -21,19 +21,19 @@ print(val_df.shape)
 x_train, y_train = train_df.drop(["TransactionID","isFraud"], axis=1), train_df[["TransactionID", "isFraud"]]
 x_val, y_val = val_df.drop(["TransactionID","isFraud"], axis=1), val_df[["TransactionID", "isFraud"]]
 
-x_train = reduce_mem_usage(x_train, sparse=True)
+# x_train = reduce_mem_usage(x_train, sparse=True)
 
-x_val = reduce_mem_usage(x_val, sparse=True)
+# x_val = reduce_mem_usage(x_val, sparse=True)
 
-model = lgb.LGBMClassifier(boosting_type="goss", n_estimators=500, objective="binary", silent=False)
+model = lgb.LGBMClassifier(boosting_type="goss", n_estimators=500, objective="binary", silent=False, random_state=42)
 trained_model = model.fit(x_train, y_train["isFraud"].values)
 
-with open(f"./data/{model_file_name}") as file:
+with open(f"./data/{model_file_name}", "wb") as file:
     pickle.dump(trained_model, file)
 
-y_prob = trained_model.predict_proba(x_val, y_val)[:, 1]
-roc_auc = roc_auc_score(y_val, y_prob)
-pr_auc = average_precision_score(y_val, y_prob)
+y_prob = trained_model.predict_proba(x_val)[:, 1]
+roc_auc = roc_auc_score(y_val["isFraud"].values, y_prob)
+pr_auc = average_precision_score(y_val["isFraud"].values, y_prob)
 
 with open(f"{metrics_path}/auc.metric", "w") as file:
     file.write(f"{roc_auc: .5f}")
